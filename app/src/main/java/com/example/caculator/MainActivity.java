8de7f0a5769e8685d2901ef361624539af537ec1
namsp <namgoku235@gmail.com>
2024-10-31 23:49:48 +0700

    layout_Caculator_2

--- app/src/main/java/com/example/caculator/MainActivity.java
+++ app/src/main/java/com/example/caculator/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.caculator;
 
+import static com.example.caculator.R.*;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     Button btn_equal;
     TextView txt_input;
     TextView txt_result;
+
+    //----
+    private LinearLayout ll_number;
+    private LinearLayout ll_euation;
+    private LinearLayout ll_convert;
+    boolean  check_equation = false;
+    boolean check_convert = false;
+    private Button btn_euqation;
+    private Button btn_convert;
+    //----
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +39,14 @@ public class MainActivity extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        // --- anh xa ID ---
+        mapping();
 
-        btn_0 = (Button)  findViewById(R.id.btn_0);
-        btn_equal = (Button) findViewById(R.id.btn_equal);
-        txt_input = (TextView) findViewById(R.id.txt_input);
-        txt_result = (TextView) findViewById(R.id.txt_result);
+        // func thay doi layout Equation, Convert
+        Change_Equation();
+        Change_Convert();
+
+        // -- test thu
         txt_input.setText("");
         btn_0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,5 +62,68 @@ public class MainActivity extends AppCompatActivity {
                 txt_result.setText(txt_input.getText().toString());
             }
         });
+        //--
     }
+
+    //--- ham anh xa -------
+    public  void mapping(){
+        ll_number = findViewById(R.id.layout_number);
+        ll_euation = findViewById(R.id.layout_equation);
+        ll_convert = findViewById(id.layout_convert);
+
+        btn_euqation = findViewById(R.id.btn_equation);
+
+        btn_convert = findViewById(R.id.btn_convert);
+        //-------
+
+        btn_0 = (Button)  findViewById(R.id.btn_0);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
+        txt_input = (TextView) findViewById(R.id.txt_input);
+        txt_result = (TextView) findViewById(R.id.txt_result);
+    }
+
+
+//=============================================================
+    public void Change_Equation(){
+        btn_euqation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_equation == false){
+                    // Ẩn Layout 1 và hiện Layout 2
+                    ll_number.setVisibility(View.GONE);
+                    ll_convert.setVisibility(View.GONE);
+                    ll_euation.setVisibility(View.VISIBLE);
+                    check_equation = true;
+                }
+                else{
+                    // Ẩn Layout 2 và hiện Layout 1
+                    ll_euation.setVisibility(View.GONE);
+                    ll_convert.setVisibility(View.GONE);
+                    ll_number.setVisibility(View.VISIBLE);
+                    check_equation = false;
+                }
+
+            }
+        });
+    }
+    public void Change_Convert(){
+        btn_convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(check_convert == false){
+                    ll_convert.setVisibility(View.VISIBLE);
+                    ll_euation.setVisibility(View.GONE);
+                    ll_number.setVisibility(View.GONE);
+                    check_convert = true;
+                }
+                else {
+                    ll_convert.setVisibility(View.GONE);
+                    ll_euation.setVisibility(View.GONE);
+                    ll_number.setVisibility(View.VISIBLE);
+                    check_convert = false;
+                }
+            }
+        });
+    }
+
 }
